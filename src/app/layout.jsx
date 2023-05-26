2a4db1d000ea5acb983a043d647375b7d7ebf4fe
zenna-x <rosymina@gmail.com>
2023-05-26 12:04:42 +0900

    add dark mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import './globals.css';
+import Providers from './Providers';
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import MenuItem from './MenuItem';
 import { AiFillHome } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
+import DarkModeSwitch from './DarkModeSwitch';
 
 function Header() {
   return (
@@ -10,7 +11,8 @@ function Header() {
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <div className="">
+      <div className="flex items-center space-x-5">
+        <DarkModeSwitch />
         <Link href="/">
           <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
