43d8d6fda5ab31bae0728bf28e83abae7bf6b103
zenna-x <rosymina@gmail.com>
2023-05-28 23:39:56 +0900

    fetch data from tmdb api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,25 @@
-import Image from 'next/image';
+import Results from '@/components/Results';
 
-export default function Home() {
-  return <h1 className="">HOME</h1>;
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTopRared' ? 'movie/top_rated' : 'trending/all/day'
+    }?api_key=${API_KEY}&language=en&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  if (!res.ok) {
+    throw new Error('Fail to fetch data');
+  }
+
+  const data = await res.json();
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
